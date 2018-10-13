f9723fcbebf36a7aceaba382a7e1ec9bbe8e5a04
Muhammad Ali <alinaeem229@gmail.com>
2018-10-13 11:41:13 -0400

    Making successful request

--- Hackathon/Form1.cs
+++ Hackathon/Form1.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using Microsoft.Azure.CognitiveServices.Vision.ComputerVision;
+using Microsoft.Azure.CognitiveServices.Vision.ComputerVision.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -18,95 +18,64 @@ namespace Hackathon
         {
             InitializeComponent();
         }
-        public const string subscriptionKey = "d188394483444e09b42d2fa3fc0b2598";
-        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0";
-        public void Main()
-        {
-            data.AppendText("Data recieved:");
-            // Get the path and filename to process from the user.
-            Console.WriteLine("Analyze an image:");
-            Console.Write(
-                "Enter the path to the image you wish to analyze: ");
-            string imageFilePath = "C:\\Users\\AliNa\\Desktop\\HackATL\\Images\\dog.jpeg";
+        // subscriptionKey = "0123456789abcdef0123456789ABCDEF"
+        private const string subscriptionKey = "d188394483444e09b42d2fa3fc0b2598";
 
-            if (File.Exists(imageFilePath))
-            {
-                // Call the REST API method.
-                Console.WriteLine("\nWait a moment for the results to appear.\n");
-                MakeAnalysisRequest(imageFilePath).Wait();
-                Console.WriteLine("Request completed");
-            }
-            else
-            {
-                Console.WriteLine("\nInvalid file path");
-            }
-            Console.WriteLine("\nPress Enter to exit...");
+        // localImagePath = @"C:\Documents\LocalImage.jpg"
+        private const string localImagePath = @"C:\Users\AliNa\Desktop\HackATL\Images\dog.jpeg";
+
+        // Specify the features to return
+        private static readonly List<VisualFeatureTypes> features =
+            new List<VisualFeatureTypes>()
+        {
+            VisualFeatureTypes.Categories, VisualFeatureTypes.Description,
+            VisualFeatureTypes.Faces, VisualFeatureTypes.ImageType,
+            VisualFeatureTypes.Tags
+        };
+
+        private void makingRequest()
+        {
+            ComputerVisionClient computerVision = new ComputerVisionClient(
+               new ApiKeyServiceClientCredentials(subscriptionKey),
+               new System.Net.Http.DelegatingHandler[] { });
+            computerVision.Endpoint = "https://eastus.api.cognitive.microsoft.com/vision/v1.0/describe?maxCandidates=1";
+
+            Console.WriteLine("Images being analyzed ...");
+            var t2 = AnalyzeLocalAsync(computerVision, localImagePath);
+
+            Task.WhenAll(t2).Wait(5000);
+            Console.WriteLine("Press ENTER to exit");
             Console.ReadLine();
         }
-        static async Task MakeAnalysisRequest(string imageFilePath)
+
+        // Analyze a local image
+        private static async Task AnalyzeLocalAsync(
+            ComputerVisionClient computerVision, string imagePath)
         {
-            try
+            if (!File.Exists(imagePath))
             {
-
-                Console.WriteLine("test");
-                HttpClient client = new HttpClient();
-
-                // Request headers.
-                client.DefaultRequestHeaders.Add(
-                    "Ocp-Apim-Subscription-Key", subscriptionKey);
-                string requestParameters = "visualFeatures=Categories,Description,Color";
-                Console.WriteLine("test1");
-                // Assemble the URI for the REST API method.
-                string uri = uriBase + "?" + requestParameters;
-
-                HttpResponseMessage response;
-                Console.WriteLine("test2");
-                // Read the contents of the specified local image
-                // into a byte array.
-                byte[] byteData = GetImageAsByteArray(imageFilePath);
-                Console.WriteLine("test2.5");
-                // Add the byte array as an octet stream to the request body.
-                using (ByteArrayContent content = new ByteArrayContent(byteData))
-                {
-                    content.Headers.ContentType =
-                        new MediaTypeHeaderValue("application/octet-stream");
-
-                    // Asynchronously call the REST API method.
-                    response = await client.PostAsync(uri, content);
-                }
-                Console.WriteLine("test3");
-                // Asynchronously get the JSON response.
-                string contentString = await response.Content.ReadAsStringAsync();
-                 Console.WriteLine("test4");
-                // Display the JSON response.
-                Console.WriteLine("\nResponse:\n\n{0}\n");
-                    //JToken.Parse(contentString).ToString());
+                Console.WriteLine(
+                    "\nUnable to open or read localImagePath:\n{0} \n", imagePath);
+                return;
             }
-            catch (Exception e)
+
+            using (Stream imageStream = File.OpenRead(imagePath))
             {
-                Console.WriteLine("\n" + e.Message);
+                ImageAnalysis analysis = await computerVision.AnalyzeImageInStreamAsync(
+                    imageStream, features);
+                DisplayResults(analysis, imagePath);
             }
         }
 
-        /// <summary>
-        /// Returns the contents of the specified file as a byte array.
-        /// </summary>
-        /// <param name="imageFilePath">The image file to read.</param>
-        /// <returns>The byte array of the image data.</returns>
-        static byte[] GetImageAsByteArray(string imageFilePath)
+        // Display the most relevant caption for the image
+        private static void DisplayResults(ImageAnalysis analysis, string imageUri)
         {
-            // Open a read-only file stream for the specified file.
-            using (FileStream fileStream =
-                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
-            {
-                // Read the file's contents into a byte array.
-                BinaryReader binaryReader = new BinaryReader(fileStream);
-                return binaryReader.ReadBytes((int)fileStream.Length);
-            }
+            Console.WriteLine(imageUri);
+            Console.WriteLine(analysis.Description.Captions[0].Text + "\n");
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            Main();
+            makingRequest();
         }
     }
 }
