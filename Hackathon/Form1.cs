7b81e9f68e4588c49ba0e7faf38f92380b3a0550
Muhammad Ali <alinaeem229@gmail.com>
2018-10-13 11:19:21 -0400

    Trying post

--- Hackathon/Form1.cs
+++ Hackathon/Form1.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace Hackathon
 {
     public partial class Form1 : Form
@@ -16,26 +18,95 @@ namespace Hackathon
         {
             InitializeComponent();
         }
-        public const string subscriptionKey = "d61126ba7857423d95f8e6ad6e8fe743";
-        private void makePost()
+        public const string subscriptionKey = "d188394483444e09b42d2fa3fc0b2598";
+        const string uriBase = "https://westcentralus.api.cognitive.microsoft.com/vision/v2.0";
+        public void Main()
         {
-            var values = new Dictionary<string, string>
+            data.AppendText("Data recieved:");
+            // Get the path and filename to process from the user.
+            Console.WriteLine("Analyze an image:");
+            Console.Write(
+                "Enter the path to the image you wish to analyze: ");
+            string imageFilePath = "C:\\Users\\AliNa\\Desktop\\HackATL\\Images\\dog.jpeg";
+
+            if (File.Exists(imageFilePath))
+            {
+                // Call the REST API method.
+                Console.WriteLine("\nWait a moment for the results to appear.\n");
+                MakeAnalysisRequest(imageFilePath).Wait();
+                Console.WriteLine("Request completed");
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid file path");
+            }
+            Console.WriteLine("\nPress Enter to exit...");
+            Console.ReadLine();
+        }
+        static async Task MakeAnalysisRequest(string imageFilePath)
         {
-            { "thing1", "hello" },
-                 { "thing2", "world" }
-               };
+            try
+            {
 
-            var content = new FormUrlEncodedContent(values);
+                Console.WriteLine("test");
+                HttpClient client = new HttpClient();
 
-            var response = await client.PostAsync("http://www.example.com/recepticle.aspx", content);
+                // Request headers.
+                client.DefaultRequestHeaders.Add(
+                    "Ocp-Apim-Subscription-Key", subscriptionKey);
+                string requestParameters = "visualFeatures=Categories,Description,Color";
+                Console.WriteLine("test1");
+                // Assemble the URI for the REST API method.
+                string uri = uriBase + "?" + requestParameters;
 
-            var responseString = await response.Content.ReadAsStringAsync();
+                HttpResponseMessage response;
+                Console.WriteLine("test2");
+                // Read the contents of the specified local image
+                // into a byte array.
+                byte[] byteData = GetImageAsByteArray(imageFilePath);
+                Console.WriteLine("test2.5");
+                // Add the byte array as an octet stream to the request body.
+                using (ByteArrayContent content = new ByteArrayContent(byteData))
+                {
+                    content.Headers.ContentType =
+                        new MediaTypeHeaderValue("application/octet-stream");
+
+                    // Asynchronously call the REST API method.
+                    response = await client.PostAsync(uri, content);
+                }
+                Console.WriteLine("test3");
+                // Asynchronously get the JSON response.
+                string contentString = await response.Content.ReadAsStringAsync();
+                 Console.WriteLine("test4");
+                // Display the JSON response.
+                Console.WriteLine("\nResponse:\n\n{0}\n");
+                    //JToken.Parse(contentString).ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n" + e.Message);
+            }
+        }
+
+        /// <summary>
+        /// Returns the contents of the specified file as a byte array.
+        /// </summary>
+        /// <param name="imageFilePath">The image file to read.</param>
+        /// <returns>The byte array of the image data.</returns>
+        static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            // Open a read-only file stream for the specified file.
+            using (FileStream fileStream =
+                new FileStream(imageFilePath, FileMode.Open, FileAccess.Read))
+            {
+                // Read the file's contents into a byte array.
+                BinaryReader binaryReader = new BinaryReader(fileStream);
+                return binaryReader.ReadBytes((int)fileStream.Length);
+            }
         }
         private void Form1_Load(object sender, EventArgs e)
         {
-            
-            data.AppendText("Data recieved:");
-
+            Main();
         }
     }
 }
